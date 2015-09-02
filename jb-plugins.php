5ac08e36f63fe754eae7b7258a9cd17e072febc1
james <james@responsivedeveloper.com>
2015-09-02 21:47:25 +0000

    basic plugin working base code added, this is to test the concept of admin panel for the plugin

--- jb-plugins.php
+++ jb-plugins.php
@@ -9,7 +9,75 @@ Plugin URI: http://git.responsivedeveloper.com/bob/
 Description: jb-plugins
 Author: Bob Toovey and James Mcavady
 Version: 0.1
-Author URI: https://responsivedeveloper.com/ http://buisness-gears.co.uk/
+Author URI: http://buisness-gears.co.uk/
 */
+
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+add_action('admin_menu', 'jbplugins_admin_actions');
+
+function jbplugins_admin_actions() {
+	add_options_page('recipes', 'Add recipes', 'manage_options', __FILE__, 'recipes_admin');
+}
+
+function recipes_admin() {
+?>
+ <div class='addrecipe' style='float:left; text-align: center; width: 97.6%; background: rgb(255, 255, 255) none repeat scroll 0% 0%; margin-top: 20px;'>
+	<h2>Add your recipes</h2>
+	<p>add recipe form</p>
+ </div>
+ <div class='listrecipe' style='float:left; text-align: center; width: 97.6%; background: rgb(255, 255, 255) none repeat scroll 0% 0%; margin-top: 20px;'>
+	<h2>Your recipes</h2>
+	<p>A list of recipes</p>
+	<table class="widefat">
+		<thead>
+		  <thead>
+		  <tr>
+		  <th>Recipe ID</th>
+		  <th>Recipe Name</th>
+		  <th>Recipe Description</th>
+		  </tr>
+		</thead>
+
+		<tbody>
+
+		<?php
+			global $wpdb;
+
+			$recipe_table = $wpdb->prefix . 'recipe'; //Good practice
+			$recipes = $wpdb->get_results( "SELECT * FROM $recipe_table");
+		?>
+
+		<?php
+			foreach ($recipes as $recipe) {
+		?>
+
+		<tr>
+
+		<?php
+			echo "<td>".$recipe->id."</td>";
+			echo "<td>".$recipe->recipe_title."</td>";
+			echo "<td>".$recipe->description."</td>";
+		?>
+
+		</tr>
+		<?php
+		}
+		?>
+		</tbody>
+
+
+
+		<tfoot>
+		  <thead>
+		  <tr>
+		  <th>Recipe ID</th>
+		  <th>Recipe Name</th>
+		  </tr>
+		</tfoot>
+	<table>
+ </div>
+
+<?php
+}
 ?>
