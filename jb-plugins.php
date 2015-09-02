8c87b628dd7a579e8be4321112c59159bbaa5bdf
james <james@responsivedeveloper.com>
2015-09-02 11:09:26 +0000

    plugin base

---
+++ jb-plugins.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package jb-plugins
+ * @version 1.6
+ */
+/*
+Plugin Name: jb-plugins
+Plugin URI: http://git.responsivedeveloper.com/bob/
+Description: jb-plugins
+Author: Bob Toovey and James Mcavady
+Version: 0.1
+Author URI: https://responsivedeveloper.com/ http://buisness-gears.co.uk/
+*/
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+?>
