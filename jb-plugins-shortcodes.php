5a8a04a81ed79ef2a79dde67b047693b7a06eb7a
bobtoovey <bobtoovey@gmail.com>
2015-09-03 14:16:45 +0200

    Upated code for testing
    Code changed so that passed values from the code will show up in the
    blog post. Good for testing!

--- jb-plugins-shortcodes.php
+++ jb-plugins-shortcodes.php
@@ -1,14 +1,10 @@
 <?php
 //rework this example to get the relivant html json code from the DB and output it correctly for each recipe post.
 
-// [bartag foo="foo-value"]
+// [bartag foo="foo-value"]    [recipetag theme="default" json="null"]
 function recipetag_func( $atts ) {
-    $a = shortcode_atts( array(
-        'foo' => 'json string from DB',
-        'bar' => 'something else',
-    ), $atts );
-
-    return "{$a['foo']}";
+    extract(shortcode_atts( array('theme' => 'default','json' => 'default',), $atts );
+    return "<div>theme:".$theme." / json:".$json."</div>";
 }
 add_shortcode( 'recipetag', 'recipetag_func' );
 ?>
