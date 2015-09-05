d1df40fb3634bc12cb3bf2c24887b5d5e3193c60
james <git.demos>
2015-09-05 14:13:15 +0100

    added further fields to the popup in the admin panel

--- jb-plugins-recipe-popup.php
+++ jb-plugins-recipe-popup.php
@@ -5,18 +5,20 @@
 ?>
 <LINK href="../wp-content/plugins/jb-plugins/css/popup.css" rel="stylesheet" type="text/css">
 <div id="messagepop">
-	<div class="popup-inner">
-		<br />
-		<form>
-			<br />
-			<div class="fieldholder"><p>Recipe Name</p><input type="textbox" name="recipename"></div>
-			<br />
-			<div class="fieldholder"><p>Recipe Description</p><input type="textbox" name="recipedescription"></div>
-			<br />
-			<div class="fieldholder"><p>Recipe Steps</p><input type="textbox" name="recipesteps"></div>
-			<br />
-			<input class="submitbutton" action="" type="submit" name="addrecipe">
-		</form>
-	</div>
+  <div class="popup-inner">
+    <h2>Add Your Recipe Below</h2>
+      <form>
+	<br />
+	<div class="fieldholder"><p>Recipe Name</p><input type="textbox" name="recipename"></div>
+	<br />
+	<div class="fieldholder"><p>Recipe Description</p><input type="textbox" name="recipedescription"></div>
+	<br />
+	<div class="fieldholder"><p>Recipe ingrediants</p><input type="textbox" name="recipesingrediants"></div>
+	<br />
+	<div class="fieldholder"><p>Recipe Steps</p><input type="textbox" name="recipesteps"></div>
+	<br />
+	<input class="submitbutton" action="" type="submit" name="addrecipe">
+      </form>
+  </div>
 </div>
 
